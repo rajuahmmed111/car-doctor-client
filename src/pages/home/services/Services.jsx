5b7781b2e18364ec1f8143a055fc7a942775e258
Raju Ahmmed <ahraju2003@gmail.com>
2025-01-04 13:07:41 +0600

    connection to database

--- src/pages/home/services/Services.jsx
+++ src/pages/home/services/Services.jsx
@@ -5,7 +5,7 @@ const Services = () => {
   const [services, setServices] = useState();
 
   useEffect(() => {
-    fetch("services.json")
+    fetch("http://localhost:5000/services")
       .then((response) => response.json())
       .then((data) => setServices(data))
       .catch((error) => console.error("Error:", error));
