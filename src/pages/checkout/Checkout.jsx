1184e05335b4d65e7960c33d49df497bc1b5ceed
Raju Ahmmed <ahraju2003@gmail.com>
2025-01-04 17:07:32 +0600

    update

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -1,9 +1,74 @@
-
+import { useLoaderData } from "react-router-dom";
 
 const Checkout = () => {
+  const service = useLoaderData();
+  const { title, price, description } = service;
+
   return (
     <div>
-      <h3>BooK service:</h3>
+      <h3>BooK service:{title}</h3>
+
+      <form className="card-body">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+            />
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+            />
+          </div>
+        </div>
+
+        <div className="form-control mt-6">
+          <input
+            type="submit"
+            value="Order Conform"
+            className="btn btn-block bg-orange-600 text-white "
+          />
+        </div>
+      </form>
     </div>
   );
 };
