05753b0653e33d19b0f72d46fc8abbeceb2d8f45
Raju Ahmmed <ahraju2003@gmail.com>
2025-01-19 17:15:50 +0600

    update

--- src/Routers/Route.jsx
+++ src/Routers/Route.jsx
@@ -27,7 +27,11 @@ const router = createBrowserRouter([
       },
       {
         path: "checkout/:id",
-        element: <BookServices />,
+        element: (
+          <PrivateRoute>
+            <BookServices />
+          </PrivateRoute>
+        ),
         loader: ({ params }) =>
           fetch(`http://localhost:5000/services/${params.id}`),
       },
