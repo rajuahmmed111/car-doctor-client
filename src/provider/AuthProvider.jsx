9b131bd3bb774ca797c69865b59c77edabc5a8ce
Raju Ahmmed <ahraju2003@gmail.com>
2025-01-19 21:40:32 +0600

    update login

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -38,7 +38,7 @@ const AuthProvider = ({ children }) => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
       setLoading(false);
-      console.log(currentUser, "currentUser");
+      // console.log(currentUser, "currentUser");
     });
 
     return () => {
