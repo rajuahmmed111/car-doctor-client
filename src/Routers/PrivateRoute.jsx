2cf07ef5e6245445f019f27d835fee4e31a3363a
Raju Ahmmed <ahraju2003@gmail.com>
2025-01-19 18:15:55 +0600

    updatae

--- src/Routers/PrivateRoute.jsx
+++ src/Routers/PrivateRoute.jsx
@@ -1,10 +1,13 @@
 import { useContext } from "react";
 import { AuthContext } from "../provider/AuthProvider";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 // eslint-disable-next-line react/prop-types
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
+
+  const location = useLocation();
+  console.log(location)
 
   if (loading) {
     return (
