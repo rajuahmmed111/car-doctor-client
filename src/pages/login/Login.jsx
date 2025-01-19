560aaaeafdbb6042b0d4cd4027ecf3976c6bd046
Raju Ahmmed <ahraju2003@gmail.com>
2025-01-19 21:07:26 +0600

    update login using jwt

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,13 +1,15 @@
-import { Link, useLocation, useNavigate } from "react-router-dom";
+// import { Link, useLocation, useNavigate } from "react-router-dom";
 import loginImg from "../../assets/images/login/login.svg";
 import { useContext } from "react";
 import { AuthContext } from "../../provider/AuthProvider";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
   const { signIn } = useContext(AuthContext);
 
-  const location = useLocation();
-  const navigate = useNavigate();
+  // const location = useLocation();
+  // const navigate = useNavigate();
 
   const handleSignIn = (e) => {
     e.preventDefault();
@@ -18,10 +20,15 @@ const Login = () => {
 
     signIn(email, password)
       .then((result) => {
-        const user = result.user;
-        console.log(user);
+        const loggedInUser = result.user;
+        console.log(loggedInUser);
+        // navigate(location?.state ? location?.state : "/");
 
-        navigate(location?.state ? location?.state : "/");
+        // get access token
+        const user = { email };
+        axios.post("http://localhost:5000/jwt", user).then((res) => {
+          console.log(res.data);
+        });
       })
       .catch((err) => {
         console.error("Error creating user:", err);
