ec584e73192c7d8dbac14910e70f0c106fd0dfca
Raju Ahmmed <ahraju2003@gmail.com>
2025-01-26 23:05:55 +0600

    update logout functionality

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -37,18 +37,25 @@ const AuthProvider = ({ children }) => {
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       const userEmail = currentUser?.email || user?.email;
-
+      const loggedUser = { email: userEmail };
       setUser(currentUser);
       setLoading(false);
 
       if (currentUser) {
-        const loggedUser = { email: userEmail };
         axios
           .post("http://localhost:5000/jwt", loggedUser, {
             withCredentials: true,
           })
           .then((res) => {
             console.log("token response", res.data);
+          });
+      } else {
+        axios
+          .post("http://localhost:5000/logout", loggedUser, {
+            withCredentials: true,
+          })
+          .then((res) => {
+            console.log("logout response", res.data);
           });
       }
     });
