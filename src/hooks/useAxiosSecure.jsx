beb29e865fe73eebb535badfa707f705cde0a5fd
Raju Ahmmed <ahraju2003@gmail.com>
2025-01-27 17:05:13 +0600

    updfate

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,4 +1,7 @@
 import axios from "axios";
+import { useEffect } from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
 
 const axiosSecure = axios.create({
   baseURL: "http://localhost:5000",
@@ -6,6 +9,25 @@ const axiosSecure = axios.create({
 });
 
 const useAxiosSecure = () => {
+  const { logOut } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axiosSecure.interceptors.response.use(
+      (res) => {
+        return res;
+      },
+      (error) => {
+        console.log("error tracked in interceptors ", error.response);
+        if (error.response.status === 401 || error.response.status === 403 ) {
+          console.log("logout user");
+          logOut().then(() => {
+            navigate("/login");
+          });
+        }
+      }
+    );
+  }, []);
   return axiosSecure;
 };
 
